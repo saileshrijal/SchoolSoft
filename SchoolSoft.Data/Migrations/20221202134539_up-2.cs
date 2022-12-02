53cb394c4a8078d5949231499ed333059fb71854
Abhishek Bohora <abhishek.bohora16@gmail.com>
2022-12-02 19:33:58 +0545

    update-faculty-2

---
+++ SchoolSoft.Data/Migrations/20221202134539_up-2.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolSoft.Data.Migrations
+{
+    public partial class up2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "Faculties",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "description",
+                table: "Faculties",
+                newName: "Description");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Faculties",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Faculties",
+                newName: "name");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Faculties",
+                newName: "description");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Faculties",
+                newName: "id");
+        }
+    }
+}
--- SchoolSoft.Models/Faculty.cs
+++ SchoolSoft.Models/Faculty.cs
@@ -8,8 +8,8 @@ namespace SchoolSoft.Models
 {
     public class Faculty
     {
-        public int id { get; set; } 
-        public string? name { get; set; }
-        public string? description { get; set; }
+        public int Id { get; set; } 
+        public string? Name { get; set; }
+        public string? Description { get; set; }
     }
 }
