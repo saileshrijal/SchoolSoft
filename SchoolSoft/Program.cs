9b206361b908d7b0dd96c220975cbac6424671d5
Sailesh Rijal <saileshrijal1@gmail.com>
2022-12-09 18:16:07 +0545

    ui added

--- SchoolSoft/Program.cs
+++ SchoolSoft/Program.cs
@@ -50,6 +50,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 
 app.Run();
